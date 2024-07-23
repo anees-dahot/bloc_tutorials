5feb0b4acdf790814c7548e5f734bb7287d49a6a
Anees <aneesdahot2020@gmail.com>
2024-07-23 16:45:42 +0500

    Bloc test for counter aoo

--- lib/counter app/cubit/counter_state.dart
+++ lib/counter app/cubit/counter_state.dart
@@ -1,11 +1,15 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'counter_cubit.dart';
 
-class CounterState {
+class CounterState extends Equatable {
   int counterValue;
   bool wasIncremented;
   CounterState({
     required this.counterValue,
     this.wasIncremented = false,
   });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [this.counterValue, this.wasIncremented];
 }
---
+++ test/counter app/cubit/counter_cubit_test.dart
@@ -0,0 +1,35 @@
+import 'package:bloc_practice/counter%20app/cubit/counter_cubit.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('CounterCubit', () {
+    CounterCubit? counterCubit;
+
+    setUp(() {
+      counterCubit = CounterCubit();
+    });
+    tearDown(() {
+      counterCubit!.close();
+    });
+
+    test(
+        'The initial state for the CounterCubit is CounterState(counterValue:0)',
+        () {
+      expect(counterCubit!.state, CounterState(counterValue: 0));
+    });
+
+    blocTest(
+      'emits CounterState(counterValue: 1, wasIncremented = true) when cubit.inrement() function is called.',
+      build: () => CounterCubit(),
+      act: (cubit) => cubit.increment(),
+      expect: () => [CounterState(counterValue: 1, wasIncremented: true)],
+    );
+    blocTest(
+      'emits CounterState(counterValue: -1, wasIncremented = false) when cubit.decrement() function is called.',
+      build: () => CounterCubit(),
+      act: (cubit) => cubit.decrement(),
+      expect: () => [CounterState(counterValue: -1, wasIncremented: false)],
+    );
+  });
+}
