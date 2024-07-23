dcb71630a399f4a172eed47d5340d516a2092c59
Anees <aneesdahot2020@gmail.com>
2024-07-23 16:05:22 +0500

    Counter app with bloc

---
+++ lib/counter app/counter_screen.dart
@@ -0,0 +1,86 @@
+import 'package:bloc_practice/counter%20app/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            BlocConsumer<CounterCubit, CounterState>(
+             listener: (context, state) {
+         if(state.wasIncremented == true){
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text( 'Counter value inremented'), duration: Duration(microseconds: 100)));
+         }
+         else{
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text( 'Counter value decremented'), duration: Duration(microseconds: 100),));
+         }
+        },
+              builder: (context, state) {
+                if (state.counterValue == 2) {
+                  return Text(
+                    'OHHH, an even ${state.counterValue.toString()}',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                }
+                else if (state.counterValue < 0) {
+                  return Text(
+                    'BRRRR, Negtive ${state.counterValue.toString()}',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                }
+               else if (state.counterValue == 5) {
+                  return Text(
+                    'DANG, a Five!!',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                }
+                return Text(
+                  state.counterValue.toString(),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
+            ),
+            SizedBox(
+              height: 43,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                  },
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.add),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  tooltip: 'Decrement',
+                  child: const Icon(Icons.remove),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:bloc_practice/counter%20app/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'counter app/counter_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
